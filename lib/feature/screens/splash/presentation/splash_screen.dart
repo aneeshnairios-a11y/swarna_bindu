b4d4854c142495a91a95207772219cfa647e1bb3
Aman Singh <harshalpatil1117@gmail.com>
2026-07-11 06:29:09 +0530

    Update splash_screen.dart

--- lib/feature/screens/splash/presentation/splash_screen.dart
+++ lib/feature/screens/splash/presentation/splash_screen.dart
@@ -39,7 +39,7 @@ class _SplashScreenState extends State<SplashScreen>
     _controller.forward();
 
     _navigationTimer = Timer(const Duration(milliseconds: 2400), () {
-      if (mounted) context.go(RouteName.dashboard);
+      if (mounted) context.go(RouteName.login);
     });
   }
 
